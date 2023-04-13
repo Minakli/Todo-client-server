3e07e5913254788c5267b4dbe2d49ab28d8d1e45
Minakli <ges-a@ya.ru>
2023-04-13 17:26:56 +0530

    The files have been fixed.

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,52 @@
+//Header
 const headerWindow = document.querySelector(".header__window");
 const headerForm = document.querySelector(".header__form");
 const headerInput = document.querySelector(".header__input");
 const headerBtnAdd = document.querySelector(".header__btn_add");
+//Main
+const main = document.querySelector(".main");
 const mainTaskList = document.querySelector(".main__task_list");
+
+//Close menu
 const closeMenu = document.querySelector(".close__menu");
 const closeSelectedBtn = document.querySelector(".close_selected_btn");
 const closeAllBtn = document.querySelector(".close__all_btn");
+
 //Generator id
 let numId = 0;
 localStorage.getItem("numId") ? (numId = localStorage.getItem("numId")) : null;
 
+//Cansel reload because form
 headerForm.addEventListener("submit", (event) => {
   event.preventDefault();
 });
+
 //Create task array
 let tasks = [];
 localStorage.getItem("tasks")
   ? (tasks = JSON.parse(localStorage.getItem("tasks")))
   : null;
-//Show tasks
+
+//Show tasks after reload
 tasks.forEach((elem) => {
   createTask();
   showTask(elem);
+  if (JSON.parse(localStorage.getItem("tasks"))[0]) {
+    main.classList.remove("hide");
+    headerWindow.classList.add("header__window_with_main");
+  }
 });
 
-//Header btn handler
+//Add btn handler
 headerBtnAdd.addEventListener("click", () => {
-  createTask();
-  showTask(tasks[tasks.length - 1]);
+  if (headerInput.value) {
+    createTask();
+    showTask(tasks[tasks.length - 1]);
+    headerWindow.classList.add("header__window_with_main");
+    main.classList.remove("hide");
+  }
 });
+
 //Create object task
 function createTask() {
   if (headerInput.value) {
@@ -37,19 +55,16 @@ function createTask() {
       isSelected: false,
       id: "task_Id_" + numId,
     };
-    console.log(task);
     localStorage.getItem("numId")
       ? (numId = localStorage.getItem("numId"))
       : null;
     numId++;
-    console.log(numId);
     localStorage.setItem("numId", numId);
     localStorage.getItem("tasks")
       ? (tasks = JSON.parse(localStorage.getItem("tasks")))
       : null;
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log(tasks);
     headerInput.value = "";
   }
 }
@@ -58,6 +73,7 @@ function createTask() {
 function showTask(elem) {
   const taskLi = document.createElement("li");
   taskLi.classList.add("main__task_unit");
+  taskLi.setAttribute("id", elem.id);
   const taskBox = document.createElement("input");
   taskBox.classList.add("input__checkBox");
   taskBox.setAttribute("type", "checkbox");
@@ -70,18 +86,59 @@ function showTask(elem) {
   mainTaskList.append(taskLi);
   taskLi.append(taskBox, taskBody, taskBtnClose);
 
+  //Checkbox listener
   taskBox.addEventListener("change", () => {
     taskBody.classList.toggle("task__body_isChecked");
     taskBody.classList.toggle("task__body");
-    if (taskBox.checked) {
-      elem.isSelected = true;
-    } else {
-      elem.isSelected = false;
-    }
+    let currentObj = tasks.find((item) => item.id === taskLi.id);
+    taskBox.checked
+      ? (currentObj.isSelected = true)
+      : (currentObj.isSelected = false);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log(elem.isSelected);
+  });
+
+  //Close single listener
+  taskBtnClose.addEventListener("click", () => {
+    taskLi.remove();
+    console.log(taskLi);
+    let index = tasks.findIndex((item) => elem.id === item.id);
+    // console.log(x);
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    if (!JSON.parse(localStorage.getItem("tasks"))[0]) {
+      main.classList.add("hide");
+      headerWindow.classList.remove("header__window_with_main");
+    }
   });
 }
+
+//Close selected
+closeSelectedBtn.addEventListener("click", () => {
+  let forDelete = tasks.filter((item) => item.isSelected === true);
+  forDelete.forEach((item) => {
+    // console.log(document.getElementById(item.id));
+    document.getElementById(item.id).remove();
+  });
+  tasks = tasks.filter((item) => item.isSelected === false);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  if (!tasks[0]) {
+    numId = 0;
+    localStorage.setItem("numId", numId);
+  }
+  if (!JSON.parse(localStorage.getItem("tasks"))[0]) {
+    // console.log(JSON.parse(localStorage.getItem("tasks")));
+    main.classList.add("hide");
+    headerWindow.classList.remove("header__window_with_main");
+  }
+});
+
+//Close all
+closeAllBtn.addEventListener("click", () => {
+  mainTaskList.innerHTML = "";
+  localStorage.clear();
+  main.classList.add("hide");
+  headerWindow.classList.remove("header__window_with_main");
+});
 // async function taskDrow() {
 //   let response = await fetch("http://localhost:3000");
 //   let objList = await response.json();
@@ -131,11 +188,3 @@ function showTask(elem) {
 // taskDrow();
 // });
 // taskDrow();
-closeSelectedBtn.addEventListener("click", () => {});
-closeAllBtn.addEventListener("click", () => {
-  while (document.querySelector("li")) {
-    document.querySelector("li").remove();
-    localStorage.clear();
-    //innerHTML
-  }
-});
